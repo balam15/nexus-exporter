e22f0e1dea8feef78b513232a31b54dc99c16f27
kamaludin1908 <126642055+kamaludin1908@users.noreply.github.com>
2025-06-24 10:52:28 +0700

    Update main.go

--- main.go
+++ main.go
@@ -14,29 +14,6 @@ import (
 )
 
 var (
-	nexusRepoCount = prometheus.NewGauge(
-		prometheus.GaugeOpts{
-			Name: "nexus_repositories_count",
-			Help: "Total number of repositories in Nexus",
-		},
-	)
-
-	nexusRepoSize = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "nexus_repositories_size_bytes",
-			Help: "Size of the repository in bytes",
-		},
-		[]string{"name"},
-	)
-
-	nexusRepoLastDownloadAge = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "nexus_repositories_last_download_age_day",
-			Help: "Age in days since the last download",
-		},
-		[]string{"name"},
-	)
-
 	nexusBlobstoreCount = prometheus.NewGauge(
 		prometheus.GaugeOpts{
 			Name: "nexus_blobstores_count",
@@ -79,9 +56,6 @@ var (
 
 func init() {
 	prometheus.MustRegister(
-		nexusRepoCount,
-		nexusRepoSize,
-		nexusRepoLastDownloadAge,
 		nexusBlobstoreCount,
 		nexusBlobstoreSize,
 		nexusBlobstoreUsed,
@@ -115,14 +89,6 @@ func fetchMetrics() {
 
 	client := &http.Client{Timeout: 15 * time.Second}
 
-	// === Repositories ===
-	repos := []map[string]interface{}{}
-	if err := fetchJSON(client, baseURL+"/service/rest/v1/repositories", username, password, &repos); err != nil {
-		log.Println("Failed to fetch repositories:", err)
-		return
-	}
-	nexusRepoCount.Set(float64(len(repos)))
-
 	// === Blobstores ===
 	blobstores := []map[string]interface{}{}
 	if err := fetchJSON(client, baseURL+"/service/rest/v1/blobstores", username, password, &blobstores); err != nil {
@@ -132,12 +98,10 @@ func fetchMetrics() {
 	nexusBlobstoreCount.Set(float64(len(blobstores)))
 	for _, bs := range blobstores {
 		name := bs["name"].(string)
-		totalSize := getFloat(bs["totalSizeInBytes"])
+		usedSpace := getFloat(bs["totalSizeInBytes"])
 		available := getFloat(bs["availableSpaceInBytes"])
+		fullSize := usedSpace + available
 		fileCount := getFloat(bs["blobCount"])
-
-		usedSpace := totalSize
-		fullSize := totalSize + available
 
 		usagePercent := 0.0
 		if fullSize > 0 {
@@ -148,44 +112,6 @@ func fetchMetrics() {
 		nexusBlobstoreUsed.WithLabelValues(name).Set(usedSpace)
 		nexusBlobstoreUsage.WithLabelValues(name).Set(usagePercent)
 		nexusBlobstoreFileCount.WithLabelValues(name).Set(fileCount)
-	}
-
-	// === Assets ===
-	assets, err := fetchAllAssets(client, baseURL, username, password)
-	if err != nil {
-		log.Println("Failed to fetch assets:", err)
-		return
-	}
-
-	repoSize := make(map[string]float64)
-	repoAge := make(map[string]float64)
-	repoAgeCount := make(map[string]int)
-	now := time.Now()
-
-	for _, asset := range assets {
-		repo := asset["repository"].(string)
-
-		if sz, ok := asset["size"].(float64); ok {
-			repoSize[repo] += sz
-		}
-
-		if lastDLStr, ok := asset["lastDownloaded"].(string); ok {
-			if lastDL, err := time.Parse(time.RFC3339, lastDLStr); err == nil {
-				ageDays := now.Sub(lastDL).Hours() / 24
-				repoAge[repo] += ageDays
-				repoAgeCount[repo]++
-			}
-		}
-	}
-
-	for repo, size := range repoSize {
-		nexusRepoSize.WithLabelValues(repo).Set(size)
-	}
-
-	for repo, ageTotal := range repoAge {
-		if count := repoAgeCount[repo]; count > 0 {
-			nexusRepoLastDownloadAge.WithLabelValues(repo).Set(ageTotal / float64(count))
-		}
 	}
 }
 
@@ -198,42 +124,6 @@ func fetchJSON(client *http.Client, url, user, pass string, target interface{}) 
 	}
 	defer resp.Body.Close()
 	return json.NewDecoder(resp.Body).Decode(target)
-}
-
-func fetchAllAssets(client *http.Client, baseURL, user, pass string) ([]map[string]interface{}, error) {
-	var assets []map[string]interface{}
-	token := ""
-
-	for {
-		url := baseURL + "/service/rest/v1/search/assets"
-		if token != "" {
-			url += "?continuationToken=" + token
-		}
-
-		req, _ := http.NewRequest("GET", url, nil)
-		req.SetBasicAuth(user, pass)
-		resp, err := client.Do(req)
-		if err != nil {
-			return nil, err
-		}
-		defer resp.Body.Close()
-
-		var result map[string]interface{}
-		if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
-			return nil, err
-		}
-
-		for _, item := range result["items"].([]interface{}) {
-			assets = append(assets, item.(map[string]interface{}))
-		}
-
-		if next, ok := result["continuationToken"].(string); ok && next != "" {
-			token = next
-		} else {
-			break
-		}
-	}
-	return assets, nil
 }
 
 func getFloat(val interface{}) float64 {
